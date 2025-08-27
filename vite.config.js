933e5638620ff071856e7d52e2377e93d45fddee
Morjane6 <131974844+Morjane6@users.noreply.github.com>
2025-08-27 21:20:51 +0200

    Oppdatert nettsiden

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Portfolio-Mohammed-Morjane",
+  base: "/Portfolio-Mohammed-Morjane/", // must match repo name
 })
