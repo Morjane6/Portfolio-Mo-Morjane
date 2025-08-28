887bb4cc900ac7e58e16156089b9e5056027bf4e
Morjane6 <131974844+Morjane6@users.noreply.github.com>
2025-08-28 12:19:07 +0200

    Oppdatert tekst/design/prosjekter

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,7 +3,7 @@ import "../../css/HeroSection.css";
 export default function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
-      <div className="hero--section--content--box">
+      <div className="hero--section--content-box">
         <div className="hero--section--content">
           <p className="section--title">Hei, Jeg er Mohammed Morjane</p>
           <h1 className="hero--section--title">
@@ -17,28 +17,33 @@ export default function HeroSection() {
           </p>
         </div>
 
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            window.location.href = "mailto:mohammed.morjane@hotmail.com";
-            closeMenu && closeMenu(); // optional, only if closeMenu is defined
-          }}
-        >
-          Contact Me
-        </button>
+        {/* Knappene pakket i egen flex-container */}
+        <div className="hero--section--buttons">
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              window.location.href = "mailto:mohammed.morjane@hotmail.com";
+            }}
+          >
+            Contact Me
+          </button>
 
-        <button
-          className="btn btn-color-1"
-          onClick={() => window.open("./img/Mohammed_Morjane_CV_2025.pdf")}
-        >
-          Download CV
-        </button>
-        <button
-          className="btn btn-github"
-          onClick={() => window.open("https://github.com/Morjane6", "_blank")}
-        >
-          Visit My GitHub
-        </button>
+          <button
+            className="btn btn-color-1"
+            onClick={() => window.open("./img/Mohammed_Morjane_CV_2025.pdf")}
+          >
+            Download CV
+          </button>
+
+          <button
+            className="btn btn-github"
+            onClick={() =>
+              window.open("https://github.com/Morjane6", "_blank")
+            }
+          >
+            Visit My GitHub
+          </button>
+        </div>
       </div>
 
       <div className="hero--section--img">
