e3c6b60a10323c5d89ab57cbbe1da27db9672eac
Morjane6 <131974844+Morjane6@users.noreply.github.com>
2025-10-15 12:25:17 +0200

    Oppdatert base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-Mohammed-Morjane/", // must match repo name
+  base: "/Portfolio-Mo-Morjane/", // must match repo name
 })
